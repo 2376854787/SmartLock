881844f5be4d74a1cf02fc281115dc6fe9571c2f
yan <2778787340@qq.com>
2025-12-05 03:18:51 +0800

    删除mqtt_at_task该任务的具体实现

--- Application/Inc/mqtt_at_task.h
+++ Application/Inc/mqtt_at_task.h
@@ -1,21 +1,8 @@
 #ifndef INC_MQTT_AT_TASK_H_
 #define INC_MQTT_AT_TASK_H_
 
-#include <stdint.h>
 
-// Reuse connection parameters from wifi_mqtt_task.h if available
-#include "wifi_mqtt_task.h"
 
-// Topic helpers for Huawei IoTDA
-#ifndef IOTDA_TOPIC_REPORT
-#define IOTDA_TOPIC_REPORT "$oc/devices/" IOTDA_DEVICE_ID "/sys/properties/report"
-#endif
-
-#ifndef IOTDA_TOPIC_DOWN
-#define IOTDA_TOPIC_DOWN   "$oc/devices/" IOTDA_DEVICE_ID "/sys/messages/down"
-#endif
-
-void StartMqttAtTask(void *argument);
 
 #endif // INC_MQTT_AT_TASK_H_
 
