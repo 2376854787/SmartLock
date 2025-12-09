d574c719cacde95fbaa03469d4ae0adc9ee20d4d
2376854787 <135600613+2376854787@users.noreply.github.com>
2025-12-09 23:37:12 +0800

    1、新增全局配置文件一键关闭项目模块

---
+++ Application/Inc/APP_config.h
@@ -0,0 +1,11 @@
+//
+// Created by yan on 2025/12/9.
+//
+
+#ifndef SMARTLOCK_APP_CONFIG_H
+#define SMARTLOCK_APP_CONFIG_H
+
+/* 功能开启宏  取消注释即开启整个项目的 log功能*/
+#define ENABLE_LOG_SYSTEM  /* 日志管理系统 */
+
+#endif //SMARTLOCK_APP_CONFIG_H
