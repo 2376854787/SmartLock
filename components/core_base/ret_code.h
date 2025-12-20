cef02c53fc0e2fd2ba602b73814827eeb34b19a4
2376854787 <135600613+2376854787@users.noreply.github.com>
2025-12-20 19:00:23 +0800

    功能函数一律返回 ret_code_t，数据通过指针参数传出。

---
+++ components/core_base/ret_code.h
@@ -0,0 +1,42 @@
+//
+// Created by yan on 2025/12/20.
+//
+
+#ifndef SMARTLOCK_RET_CODE_H
+#define SMARTLOCK_RET_CODE_H
+#include <stdint.h>
+#include "compiler_cus.h"
+#ifdef __cplusplus
+extern "C" {
+
+
+
+#endif
+
+typedef enum {
+    RET_OK = 0,
+
+    /* generic errors */
+    RET_E_FAIL = -1,
+    RET_E_INVALID_ARG = -2,
+    RET_E_TIMEOUT = -3,
+    RET_E_BUSY = -4,
+    RET_E_NO_MEM = -5,
+    RET_E_NOT_FOUND = -6,
+    RET_E_NOT_READY = -7,
+    RET_E_OVERFLOW = -8,
+    RET_E_UNSUPPORTED = -9,
+    RET_E_IO = -10,
+    RET_E_CRC = -11,
+
+    /* extend as needed... */
+} ret_code_t;
+
+__INLINE int ret_is_ok(ret_code_t r) { return (r == RET_OK); }
+__INLINE int ret_is_err(ret_code_t r) { return (r != RET_OK); }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //SMARTLOCK_RET_CODE_H
