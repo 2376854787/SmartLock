71195bb653b7423e82c7423a2d154742d5e7f4ff
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-01 02:45:28 +0800

    统一管理全局开启宏

--- components/core_base/config_cus.h
+++ components/core_base/config_cus.h
@@ -11,5 +11,17 @@
 #define ENABLE_STATIC_ALLOCATION        /* 静态内存分配 */
 #define ENABLE_RINGBUFFER_SYSTEM        /* 环形缓冲区系统 */
 #define ENABLE_HFSM_SYSTEM              /* HFSM系统 */
-#define ENABLE_KEYS
+#define ENABLE_KEYS                     /* 使能按键系统 */
+#define ENABLE_ASSERT_SYSTEM            /* 使能断言系统 */
+
+
+/* 协议标准配置宏  取消注释即开启整个项目的 执行标准*/
+/* 启动CMSIS v2 标准 */
+#define OSAL_BACKEND_CMSIS_OS2        1
+
+/* 裸机 和 RTOS环境配置宏 */
+/* 1 使用 FreeRTOS 的taskENTER_CRITICAL()/FROM_ISR
+ * 0 使用 PRIMASK  全关中断 （延时更大）
+ */
+#define OSAL_CRITICAL_IMPL_FREERTOS   1
 #endif //SMARTLOCK_CONFIG_CUS_H
