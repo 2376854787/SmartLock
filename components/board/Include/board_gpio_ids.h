d8cbfbafbc5188c80e3aa731e5b708c37abf7a55
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-14 23:29:33 +0800

    实现抽象层stm32/gpio_port 实现时钟、模式、方向、速度、引脚复用的映射。 1、重构为全局ID 映射使用GPIO

---
+++ components/board/Include/board_gpio_ids.h
@@ -0,0 +1,10 @@
+
+#ifndef SMARTLOCK_BOARD_GPIO_IDS_H
+#define SMARTLOCK_BOARD_GPIO_IDS_H
+/* 你自己定义板级 ID（建议放到 board_gpio_ids.h） */
+enum {
+    HAL_GPIO_ID_LED1 = 0,
+    HAL_GPIO_ID_BTN1 = 1,
+};
+
+#endif //SMARTLOCK_BOARD_GPIO_IDS_H
