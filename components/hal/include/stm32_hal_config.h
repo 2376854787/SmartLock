7aaa6124269355d1e39416ef0adcf709d6e352bf
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-17 23:07:04 +0800

    增加对 stm32 H7/F7 系列对于DMA有缓存 而可能导致的错误处理办法。 当使用该系列开启对应的宏

--- components/hal/include/stm32_hal_config.h
+++ components/hal/include/stm32_hal_config.h
@@ -6,4 +6,8 @@
 /* 用于开启 stm32_port 的DMA 实现函数 */
 #define USE_HAL_UARTEx_ReceiveToIdle_DMA       /* 启用 DMA + IDLE接收方式 */
 #define DISABLE_DMA_IT_HT                false /* 是否关闭 DMA 过半中断  */
-#endif                                         // HAL_CONFIG_H
+
+/* STM32 H7系列缓存优化 */
+// #define SCB_CleanDCache_by_Addr
+// #define SCB_InvalidateDCache_by_Addr
+#endif  // HAL_CONFIG_H
