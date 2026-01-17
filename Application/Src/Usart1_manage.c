5df830346ce717dcb8de863d0be84be038ebdf78
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-17 21:40:48 +0800

    RB 命令改为函数入参 创建初始化

--- Application/Src/Usart1_manage.c
+++ Application/Src/Usart1_manage.c
@@ -6,24 +6,23 @@
 
 #include <stdio.h>
 
-#include "log.h"
 #include "MemoryAllocation.h"
-#include "usart.h"
+#include "log.h"
 #include "ret_code.h"
+#include "usart.h"
 extern DMA_HandleTypeDef hdma_usart1_rx;
 extern DMA_HandleTypeDef hdma_usart1_tx;
-RingBuffer g_rb_uart1 = {
-    .name = "USART1 RingBuffer"
-};
+RingBuffer g_rb_uart1;
 
 uint8_t DmaBuffer[DMA_BUFFER_SIZE];
 
 bool MyUart_Init(void) {
-    if (ret_is_err(CreateRingBuffer(&g_rb_uart1,RINGBUFFER_SIZE))) {
+    if (ret_is_err(CreateRingBuffer(&g_rb_uart1, "USART1 RingBuffer", RINGBUFFER_SIZE))) {
         printf("环形缓冲区初始化失败");
         return false;
     }
-    LOG_W("heap", "%uKB- %u空间还剩余 %u", MEMORY_POND_MAX_SIZE, RINGBUFFER_SIZE, query_remain_size());
+    LOG_W("heap", "%uKB- %u空间还剩余 %u", MEMORY_POND_MAX_SIZE, RINGBUFFER_SIZE,
+          query_remain_size());
     HAL_UARTEx_ReceiveToIdle_DMA(&huart1, DmaBuffer, DMA_BUFFER_SIZE);
     printf("环形缓冲区初始化成功 %p\n", &g_rb_uart1);
     return true;
@@ -31,21 +30,20 @@ bool MyUart_Init(void) {
 
 void process_dma_data(void) {
     static uint16_t last_pos = 0;
-    //1、获取当前的位置
-    uint32_t curpos = DMA_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);
+    // 1、获取当前的位置
+    uint32_t curpos          = DMA_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(&hdma_usart1_rx);
 
-    uint32_t write_size = curpos - last_pos; //没有回返
-    uint32_t write_size2 = DMA_BUFFER_SIZE - last_pos; //数据回返需要写入得后面部分得长度
-    //2、判断数据是否更新
+    uint32_t write_size      = curpos - last_pos;           // 没有回返
+    uint32_t write_size2     = DMA_BUFFER_SIZE - last_pos;  // 数据回返需要写入得后面部分得长度
+    // 2、判断数据是否更新
     if (last_pos == curpos) return;
 
-
-    //3、搬运数据到环形缓冲区  //当前数据没有回返
+    // 3、搬运数据到环形缓冲区  //当前数据没有回返
     if (curpos > last_pos) {
         if (ret_is_err(WriteRingBuffer(&g_rb_uart1, &DmaBuffer[last_pos], &write_size, 0)))
             printf("写入失败\n");
     } else {
-        //数据回返
+        // 数据回返
         if (ret_is_err(WriteRingBuffer(&g_rb_uart1, &DmaBuffer[last_pos], &write_size2, 0))) {
             printf("Write Error of RingBuffer\n");
         }
@@ -53,7 +51,6 @@ void process_dma_data(void) {
         WriteRingBuffer(&g_rb_uart1, &DmaBuffer[0], &curpos, 0);
     }
 
-
-    //4、更新位置
+    // 4、更新位置
     last_pos = curpos;
 }
