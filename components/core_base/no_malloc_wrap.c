9016b9a56094097c34a4d9f5b555b0c81d13c9de
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-18 16:50:19 +0800

    增加注释

--- components/core_base/no_malloc_wrap.c
+++ components/core_base/no_malloc_wrap.c
@@ -1,26 +1,32 @@
 #include <stddef.h>
 
 #include "assert_cus.h"
-
-void *__wrap_malloc(size_t n) {
+/**
+ *
+ * @param n
+ * @return
+ * @note 必须CMAKELists.txt 定义 -wl,--warp=malloc
+ */
+// NOLINTNEXTLINE(bugprone-reserved-identifier)
+void* __wrap_malloc(size_t n) {
     (void)n;
     ASSERT_FATAL(!"malloc is forbidden");
     return NULL;
 }
-
-void __wrap_free(void *p) {
+// NOLINTNEXTLINE(bugprone-reserved-identifier)
+void __wrap_free(const void* p) {
     (void)p;
     ASSERT_FATAL(!"free is forbidden");
 }
-
-void *__wrap_realloc(void *p, size_t n) {
+// NOLINTNEXTLINE(bugprone-reserved-identifier)
+void* __wrap_realloc(const void* p, size_t n) {
     (void)p;
     (void)n;
     ASSERT_FATAL(!"realloc is forbidden");
     return NULL;
 }
-
-void *__wrap_calloc(size_t a, size_t b) {
+// NOLINTNEXTLINE(bugprone-reserved-identifier)
+void* __wrap_calloc(size_t a, size_t b) {
     (void)a;
     (void)b;
     ASSERT_FATAL(!"calloc is forbidden");
@@ -28,7 +34,8 @@ void *__wrap_calloc(size_t a, size_t b) {
 }
 
 /* alloca 有时是 builtin，不一定会走符号；wrap 只覆盖符号版本 */
-void *__wrap_alloca(size_t n) {
+// NOLINTNEXTLINE(bugprone-reserved-identifier)
+void* __wrap_alloca(size_t n) {
     (void)n;
     ASSERT_FATAL(!"alloca is forbidden");
     return NULL;
