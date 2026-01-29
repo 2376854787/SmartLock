81aa5c4e60c350f9ebcf571f040d5cc8ab1cd283
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-30 00:17:23 +0800

    新增 断言失败消息的打印

--- platform/STM32/ports/bsp_assert_port.c
+++ platform/STM32/ports/bsp_assert_port.c
@@ -2,6 +2,7 @@
 /* hal抽象选择宏 */
 #if defined(USE_STM32_HAL)
 #include "assert_cus.h"
+#include "log.h"
 #include "stm32_hal.h"
 
 /**
@@ -11,4 +12,11 @@ void Assert_PlatformReset(void) {
     NVIC_SystemReset();
 }
 
+/**
+ * @brief 覆盖实现输出断言失败的环境信息
+ * @param msg 消息来源
+ */
+void Assert_PlatformLog(const char* msg) {
+    LOG_E("Assert", msg);
+}
 #endif
