d9d361f9f4661b79b168935a6900e57aed36d6bd
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-01-30 00:24:54 +0800

    文件移动

--- components/core_base/stack/stack_monitor_port.h
+++ components/core_base/stack/stack_monitor_port.h
@@ -1,13 +1,19 @@
 #ifndef SMARTLOCK_STACK_MONITOR_PORT_H
 #define SMARTLOCK_STACK_MONITOR_PORT_H
 
-#include "stack_monitor.h"
 #include <stdint.h>
 
+#include "stack_monitor.h"
+
+/* 按照 .ldl链接脚本实际情况更改 */
 /* Linker-provided stack range symbols (see STM32F407XX_FLASH.ld). */
 extern uint32_t __stack_bottom__;
 extern uint32_t __stack_top__;
-
+/**
+ * @brief 返回封装好起始位置的栈结构体
+ * @return 栈的起始位置封装的结构体
+ * @note  必须按照 .ldl链接脚本实际情况更改
+ */
 static inline stack_range_t StackMonitor_GetMainStackRange(void) {
     stack_range_t r;
     r.stack_bottom = (uintptr_t)&__stack_bottom__;
