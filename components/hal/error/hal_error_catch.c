c407b63090ace1794ab25408d15aa60ffce8631d
2376854787 <135600613+2376854787@users.noreply.github.com>
2026-03-02 00:47:06 +0800

    feature 新增 flash 抽象的错误处理函数
    Close

--- components/hal/error/hal_error_catch.c
+++ components/hal/error/hal_error_catch.c
@@ -6,6 +6,7 @@
 #include <stddef.h>
 
 #include "assert_cus.h"
+#include "hal_flash.h"
 #include "hal_gpio.h"
 #include "hal_spi.h"
 #include <stdio.h>
@@ -96,6 +97,29 @@ void hal_wdg_on_port_error(ret_code_t rc_port, ret_code_t rc_hal, const char* ap
     if (ret_is_err(rc_port) && !OSAL_in_isr()) {
 #endif
         LOG_E("HAL_WDG", "api:%s port:0x%08lX->hal:0x%08lX arg0:%lu arg1:%lu",
+              (api != NULL) ? api : "unknown", (unsigned long)rc_port, (unsigned long)rc_hal,
+              (unsigned long)arg0, (unsigned long)arg1);
+    }
+#else
+    (void)rc_port;
+    (void)rc_hal;
+    (void)api;
+    (void)arg0;
+    (void)arg1;
+#endif
+}
+
+void hal_flash_on_port_error(ret_code_t rc_port, ret_code_t rc_hal, const char* api, uint32_t arg0,
+                             uint32_t arg1) {
+    ASSERT_PARAM(api != NULL);
+#if defined(CFG_FEAT_LOG_SYSTEM) && (CFG_FEAT_LOG_SYSTEM == 1) && \
+    defined(CFG_PARAM_FLASH_LOG_PORT_ERR) && (CFG_PARAM_FLASH_LOG_PORT_ERR == 1)
+#if defined(CFG_PARAM_FLASH_LOG_PORT_ERR_IN_ISR) && (CFG_PARAM_FLASH_LOG_PORT_ERR_IN_ISR == 1)
+    if (ret_is_err(rc_port)) {
+#else
+    if (ret_is_err(rc_port) && !OSAL_in_isr()) {
+#endif
+        LOG_E("HAL_FLASH", "api:%s port:0x%08lX->hal:0x%08lX arg0:%lu arg1:%lu",
               (api != NULL) ? api : "unknown", (unsigned long)rc_port, (unsigned long)rc_hal,
               (unsigned long)arg0, (unsigned long)arg1);
     }
